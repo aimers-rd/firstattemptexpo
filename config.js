7cb5053a699a0521f0924d97f3555c98490238e7
Ritu-whjr <suruby2k18@gmail.com>
2022-03-03 22:04:36 +0530

    first commit

---
+++ config.js
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyAf8g6pGad43B5ZdGn2UbtX6I0TNigkFCg",
+  authDomain: "storytellingapp-5a378.firebaseapp.com",
+  databaseURL: "https://storytellingapp-5a378-default-rtdb.firebaseio.com",
+  projectId: "storytellingapp-5a378",
+  storageBucket: "storytellingapp-5a378.appspot.com",
+  messagingSenderId: "909097373418",
+  appId: "1:909097373418:web:eb232583b7313d15f69193",
+  measurementId: "G-RM8DV4NGKM"
+};
